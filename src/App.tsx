f037b4a21fb9c5c9c3b11e83817363170f627ad7
Bianca Santos <biancassantos89@gmail.com>
2025-08-24 16:24:35 -0300

    style: linhas de importação organizadas

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router"
 import PublicLayout from "./layouts/PublicLayout"
 import LoggedInLayout from "./layouts/LoggedInLayout"
 import LandingPage from "./pages/landing-page/LandingPage"
-<<<<<<< HEAD
 import HistoryPage from "./pages/adoption-page/HistoryPage"
-
-=======
 import LoginPage from "./pages/login/LoginPage"
->>>>>>> e5dd7bd3441a1ca2b04c8afbd86c9f0e70813acb
 
 export default function App() {
   return (

bca80e9a57d079d4d8af1f517be4a1dd922ec294
Bianca Santos <biancassantos89@gmail.com>
2025-08-28 12:38:40 -0300

    chore: social icon movido para a pasta geral de componentes

---
+++ src/components/SocialIcon.tsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router"
+import type { ReactNode } from "react"
+
+type SocialIconProps = {
+  icon: ReactNode,
+  url: string,
+  color?: string
+}
+
+function SocialIcon({ icon, url, color = "brown" }: SocialIconProps) {
+  return (
+    <Link 
+    to={url} 
+    className={`${color === "brown" ? "text-brown hover:text-darkbrown" : "text-sand hover:text-white"} text-2xl  transition-all duration-300 p-1`}
+    >
+      {icon}
+    </Link>
+  )
+}
+
+export default SocialIcon

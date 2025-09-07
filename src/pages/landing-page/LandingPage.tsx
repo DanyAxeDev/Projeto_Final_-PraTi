349b72730599f95929ef2ca2249d537f830d14dc
Bianca Santos <biancassantos89@gmail.com>
2025-09-06 21:17:28 -0300

    feat: responsividade da landing page adicionada

--- src/pages/landing-page/LandingPage.tsx
+++ src/pages/landing-page/LandingPage.tsx
@@ -15,19 +15,19 @@ function LandingPage() {
       <PublicHeader bg={false} />
       <main className="bg-sand min-h-screen h-full pt-20">
         <div 
-        style={{ backgroundImage: "url('/backgrounds/landing-page-bg-desktop.png')" }}
-        className="h-screen bg-cover bg-bottom"
+        id="landing-page"
+        className="h-screen min-h-full bg-cover bg-bottom bg-no-repeat"
         >
-          <section className="max-w-[1100px] mx-auto flex flex-col justify-between gap-8 h-full py-12 px-8 font-raleway">
+          <section className="max-w-[1100px] mx-auto flex flex-col justify-between gap-8 h-full py-12 px-4 font-raleway sm:px-8">
             <section className="flex flex-col items-center gap-6 text-center">
-              <h1 className="font-tilt text-brown text-6xl max-w-[790px]">
+              <h1 className="font-tilt text-brown text-5xl max-w-[790px] sm:text-6xl">
                 Conectando o fio <span className="text-blue">azul</span> entre pets e seus humanos
               </h1>
-              <p className="text-2xl font-semibold mt-2 max-w-[900px]">
+              <p className="text-xl font-semibold mt-2 max-w-[900px] sm:text-2xl">
                 Textinho explicando sobre a plataforma e tudo mais de pelo menos duas linhas pra ficar mais bonitinho.
               </p>
 
-              <div className="flex gap-9 mt-8">
+              <div className="flex gap-6 mt-8 sm:gap-9">
                 <RoundButton 
                 text="Cadastre-se" 
                 color="blue" 
@@ -41,7 +41,7 @@ function LandingPage() {
               </div>
             </section>
 
-            <section className="flex justify-between">
+            <section className="flex flex-col items-center justify-between gap-10 sm:flex-row sm:items-end">
               <AdoptionStorySection />
 
               <div className="flex items-center gap-3">

a5375417665a5f7e2eaf71a950ace691b1699605
daniel <danielmac789@gmail.com>
2025-11-17 20:32:23 -0300

    add URL do back

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import type { ApiResponse } from '@/types/api';
 
-const API_BASE_URL = 'http://localhost:8080';
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 class ApiService {
     private baseURL: string;

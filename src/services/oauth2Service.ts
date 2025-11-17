c6ae1cb954a557c573e003ad7349fbbf725cc048
daniel <danielmac789@gmail.com>
2025-11-17 20:50:22 -0300

    add URL do back para o login com github

--- src/services/oauth2Service.ts
+++ src/services/oauth2Service.ts
@@ -1,6 +1,6 @@
 import type { ApiResponse, Token } from '@/types/api';
 
-const API_BASE_URL = 'http://localhost:8080';
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 export const oauth2Service = {
     /**
